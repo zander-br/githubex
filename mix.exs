9c7abb4fb501a4b24033b18475df6057abd81243
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-10 07:05:06 -0300

    chore: add bypass

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Githubex.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.17"}
+      {:hackney, "~> 1.17"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
