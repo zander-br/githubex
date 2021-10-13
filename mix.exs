ed691df212eda4fdba57aa429aaec2fc217cc871
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-13 20:51:28 -0300

    chore: add pbkdf2_elixir

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Githubex.MixProject do
       {:hackney, "~> 1.17"},
       {:bypass, "~> 2.1", only: :test},
       {:ex_machina, "~> 2.7.0", only: :test},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:pbkdf2_elixir, "~> 1.4"}
     ]
   end
 
