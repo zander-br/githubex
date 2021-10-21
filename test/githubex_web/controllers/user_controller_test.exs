46592f6c9bbbc748372bb43fee7ad83fd47f6cba
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-21 08:05:41 -0300

    test: when there is some error, returns the error

--- test/githubex_web/controllers/user_controller_test.exs
+++ test/githubex_web/controllers/user_controller_test.exs
@@ -22,5 +22,21 @@ defmodule GithubexWeb.UserControllerTest do
                }
              } = response
     end
+
+    test "when there is some error, returns the error", %{conn: conn} do
+      params = %{
+        "name" => "Joe Doe",
+        "email" => "joe.doe@mail.com"
+      }
+
+      response =
+        conn
+        |> post(Routes.user_path(conn, :create, params))
+        |> json_response(:unprocessable_entity)
+
+      expected_response = %{"errors" => %{"password" => ["can't be blank"]}}
+
+      assert response == expected_response
+    end
   end
 end
